523f2b2b8d1be2dc61e309a36a33ffd2d1896a8f
Gitesh Kumar <razor169@protonmail.com>
2022-04-24 00:10:15 +0530

    Init

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from graphql_playground.views import GraphQLPlaygroundView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False))),
+    path('playground/', GraphQLPlaygroundView.as_view(endpoint='https://locahost:8000/graphql')),
 ]
